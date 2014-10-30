5d66dac90b4cdde26d3a0f41a2f46ccddd47e6c5
KushaGharahi <kusha95@gmail.com>
2014-10-29 20:51:02 -0500

    Inital Commit.

---
+++ src/PlayingWithVectors.java
@@ -0,0 +1,11 @@
+
+public class PlayingWithVectors {
+	
+	public static void main(String args[])
+	{
+		Vector asdf = new Vector(3,4);
+		Vector asdf1 = new Vector(5,12);
+		
+		System.out.println(asdf.mult(asdf1));
+	}
+}
